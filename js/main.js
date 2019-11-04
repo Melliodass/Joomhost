ac6b38feb43c26b1207c4c14aa946450ddf63ad4
Melliodass <jet333@yandex.ru>
2019-11-04 20:36:27 +0300

    Делаем слайдер

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.rewiews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false
+    });
+
+});
